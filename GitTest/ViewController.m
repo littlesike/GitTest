7d2961087668125a61d6dc2bcdef57fbf29c859c
littlesike <515712375.com>
2016-05-26 11:09:09 +0800

    你好啊

--- GitTest/ViewController.m
+++ GitTest/ViewController.m
@@ -17,10 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
+//    注意：1.在commit的时候，是从暂存区进行提交的。在每次commit之前，都要把修改过后的文件进行add的操作，如果不进行add，在commit的时候，新修改的内容是无法提交的仓库的
+//    、、2.push的时候，是根据你commit的记录进行push，每次push之前，要先把修改过后的内容进行提交。如果不commit，直接push，导致新内容无法push到远程仓库
     
     NSLog(@"你可知你妈 爱吃烤地瓜");
     NSLog(@"第二句 啊");
+    NSLog(@"sda");
     
 }
 
